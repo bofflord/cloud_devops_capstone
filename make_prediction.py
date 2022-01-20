6ceef81bf98925d9bc962fefd30dede424a38d8d
boffmasta <boffinet@web.de>
2022-01-20 19:17:00 +0100

    completed unit test preparation

--- make_prediction.py
+++ make_prediction.py
@@ -1,38 +1,19 @@
 import requests, sys
 
-IP=sys.argv[1]
-PORT=sys.argv[2]
+def get_prediction(ip, port, parameters):
 
-api_url = f'http://{IP}:{PORT}/predict'
-print(f"api_url: {api_url}")
+    api_url = f'http://{ip}:{port}/predict'
+    print(f"api_url: {api_url}")
 
-headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 
-parameters = {  
-   "CHAS":{  
-      "0":0
-   },
-   "RM":{  
-      "0":6.575
-   },
-   "TAX":{  
-      "0":296.0
-   },
-   "PTRATIO":{  
-      "0":15.3
-   },
-   "B":{  
-      "0":396.9
-   },
-   "LSTAT":{  
-      "0":4.98
-   }
-}
 
-# sending get request and saving the response as response object
-r = requests.post(url = api_url, json = parameters, headers=headers)
 
-data = r.json()
+    # sending get request and saving the response as response object
+    r = requests.post(url = api_url, json = parameters, headers=headers)
 
-print('prediction')
-print(data['prediction'][0])
+    data = r.json()
+    prediction = data['prediction'][0]
+    print(f'prediction: {prediction}')
+    
+    return prediction
