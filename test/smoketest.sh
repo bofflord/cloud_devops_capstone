7890c8ecb11665f53837f1c35e0f50b428b5a22b
boffmasta <boffinet@web.de>
2022-01-24 18:58:21 +0100

    debugging

--- test/smoketest.sh
+++ test/smoketest.sh
@@ -3,9 +3,9 @@ PORT=$2
 echo "IP address: $IP"
 echo "port number: $PORT"
 RESULT=$(./make_prediction.sh "$IP" "$PORT" | jq .'prediction'[0])
-if $RESULT==20.35373177134412
+if [[ $RESULT=="20.35373177134412" ]]
     then 
-        echo "Smoketest passed."; exit 1
+        echo "Smoketest passed."; exit 0
     else
-        echo "Smoketest failed."; exit 0
+        echo "Smoketest failed."; exit 1
 fi
