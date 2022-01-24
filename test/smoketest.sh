d60a323eb58230316eccc2d8649e6e7e9308c084
boffmasta <boffinet@web.de>
2022-01-24 18:43:58 +0100

    created smoketest_app job and testing it

---
+++ test/smoketest.sh
@@ -0,0 +1,11 @@
+IP=$1
+PORT=$2
+echo "IP address: $IP"
+echo "port number: $PORT"
+RESULT=$(./make_prediction.sh "$IP" "$PORT" | jq .'prediction'[0])
+if [$RESULT==20.35373177134412]
+    then 
+        echo "Smoketest passed."; exit 0
+    else
+        echo "Smoketest passed."; exit 1
+fi
