040e466d8132421719a88d377f6fbd5a6086918a
boffmasta <boffinet@web.de>
2022-01-26 21:02:58 +0100

    test pylint again

--- app.py
+++ app.py
@@ -69,5 +69,5 @@ def predict():
 if __name__ == "__main__":
     # load pretrained model as clf
     clf = joblib.load("./model_data/boston_housing_prediction.joblib")
-    app.run(host='0.0.0.0', port=8080, debug=True) # specify port=8080
-    test='test'
+    app.run(host='0.0.0.0', 
+    port=8080, debug=True) # specify port=8080
